5b263f5674c922397228660f7a55b876d94d3322
Mare <2548098501@qq.com>
2023-11-04 23:18:03 +0800

    获取课程分类信息

--- src/api/course.js
+++ src/api/course.js
@@ -2,10 +2,10 @@ import request from '@/utils/request.js'
 
 export const courseCategoryService = () => {
   // /user?username=admin&password=123456
-  return request.get('/course-category')
+  return request.get('/coursecategory')
 }
 
 export const courseListService = () => {
   // /user?username=admin&password=123456
-  return request.get('/course-list')
+  return request.get('/courselist')
 }
