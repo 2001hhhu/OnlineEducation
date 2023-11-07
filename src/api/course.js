a445708bb7a05434ede2b1da7e58fa1bba9aef43
Mare <2548098501@qq.com>
2023-11-08 00:01:47 +0800

    course请求更新

--- src/api/course.js
+++ src/api/course.js
@@ -5,7 +5,23 @@ export const courseCategoryService = () => {
   return request.get('/coursecategory')
 }
 
-export const courseListService = () => {
+export const courseListService = (term, category, state) => {
   // /user?username=admin&password=123456
-  return request.get('/courselist')
+  if (term === '全部' && category === '全部' && state === '全部') {
+    return request.get('/courselist')
+  } else if (term === '全部' && category === '全部') {
+    return request.get(`/courselist?state=${state}`)
+  } else if (term === '全部' && state === '全部') {
+    return request.get(`/courselist?category=${category}`)
+  } else if (category === '全部' && state === '全部') {
+    return request.get(`/courselist?term=${term}`)
+  } else if (term === '全部') {
+    return request.get(`/courselist?category=${category}&state=${state}`)
+  } else if (category === '全部') {
+    return request.get(`/courselist?term=${term}&state=${state}`)
+  } else if (state === '全部') {
+    return request.get(`/courselist?term=${term}&category=${category}`)
+  } else {
+    return request.get(`/courselist?term=${term}&category=${category}&state=${state}`)
+  }
 }
