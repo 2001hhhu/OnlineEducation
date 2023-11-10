53e1923da1f9cdeedb0d67339106e18abb728cbf
Mare <2548098501@qq.com>
2023-11-10 22:56:40 +0800

    更新

--- src/views/layout/user/UserProfile.vue
+++ src/views/layout/user/UserProfile.vue
@@ -65,6 +65,24 @@ const handleCommmit = async () => {
   })
   console.log(userStore.user)
 }
+
+// 处理图片上传
+const imageUrl = ref('')
+
+// const handleAvatarSuccess = (response, uploadFile) => {
+//   // imageUrl.value = URL.createObjectURL(uploadFile.raw!)
+// }
+
+const beforeAvatarUpload = (rawFile) => {
+  if (rawFile.type !== 'image/jpeg') {
+    ElMessage.error('Avatar picture must be JPG format!')
+    return false
+  } else if (rawFile.size / 1024 / 1024 > 2) {
+    ElMessage.error('Avatar picture size can not exceed 2MB!')
+    return false
+  }
+  return true
+}
 </script>
 
 <template>
@@ -240,6 +258,18 @@ const handleCommmit = async () => {
                 <el-image :src="squareUrl" v-else></el-image>
               </div>
               <div>
+                <el-upload
+                  class="avatar-uploader"
+                  action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+                  :show-file-list="false"
+                  :on-success="handleAvatarSuccess"
+                  :before-upload="beforeAvatarUpload"
+                >
+                  <img v-if="imageUrl" :src="imageUrl" class="avatar-2" />
+                  <el-icon v-else class="avatar-uploader-icon"><i-ep-Plus /></el-icon>
+                </el-upload>
+              </div>
+              <div>
                 <el-button type="primary" size="small" radius>更换头像</el-button>
               </div>
             </el-form-item>
@@ -338,5 +368,29 @@ const handleCommmit = async () => {
       overflow: hidden;
     }
   }
+}
+.avatar-uploader {
+  .el-upload {
+    border: 1px dashed var(--el-border-color);
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    transition: var(--el-transition-duration-fast);
+  }
+}
+
+.avatar-uploader {
+  .el-upload:hover {
+    border-color: var(--el-color-primary);
+  }
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
 }
 </style>
