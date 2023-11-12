0befc1ba092e1e00e5d3af79af2b5a6a1939953d
Mare <2548098501@qq.com>
2023-11-12 14:01:03 +0800

    echarts按需导入

---
+++ src/utils/echarts.js
@@ -0,0 +1,32 @@
+import * as echarts from 'echarts/core'
+// 引入要用到的图表
+import { LineChart } from 'echarts/charts'
+// 引入提示框、数据集等组件
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent
+} from 'echarts/components'
+// 引入标签自动布局、全局过滤动画等特性
+import { LabelLayout, UniversalTransition } from 'echarts/features'
+// 引入Canvas渲染器，必须
+import { CanvasRenderer } from 'echarts/renderers'
+
+// 注册必须组件
+echarts.use([
+  LineChart,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  CanvasRenderer,
+  LabelLayout,
+  UniversalTransition,
+  LegendComponent
+])
+
+export default echarts
